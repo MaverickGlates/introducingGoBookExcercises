3c6033cbb0e085759b84e323cb68b0a47670bbb7
Alexander G <alexander.maverick1996@gmail.com>
2022-02-15 15:36:09 -0300

    commit for up to github

---
+++ chapter5/main.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+//In Chapter 2, we learned about GO's basic types. In this chapter, we will look at three more built-in types: arrays, slices, and maps
+
+//ARRAYS
+//An Array is a numbered sequence of elements of a single type with a fixed lenght. in GO they look like this:
+
+var x [5]int
+
+func main() {
+	fmt.Println()
+}
