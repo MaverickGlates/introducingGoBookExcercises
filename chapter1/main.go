16bd190b2c4c60c32ff1f6245bb387f63bb0dca9
Alexander G <alexander.maverick1996@gmail.com>
2022-02-16 15:58:16 -0300

    changes in chapter 1

--- chapter1/main.go
+++ chapter1/main.go
@@ -4,9 +4,15 @@ import (
 	"fmt"
 )
 
-//this is a comment
+//this is a comment, comments are not read by go. So we go to write our first program (Line 9, Line 12)
 
-// //Exercises
+func main() {
+	fmt.Println("Hello, World")
+
+}
+
+//EXCERCISES
+
 // 1. What is whitespace?
 // 2. What is a comment? What are the two ways of writing a comment?
 // 3. Our program began with package main. What would the files in the fmt package
@@ -16,13 +22,11 @@ import (
 // 5. Modify the program we wrote so that instead of printing Hello, World it prints
 // Hello, my name is followed by your name.
 
+//ANSWERS
+
 //1.-Are Special characters, that we can't see. whitespace characters, made up of space, tab. and newline do nothing in the code, we can use it to make programs easier to read
 /*2.-Comments are the form to explain a part of code, this is a good practice for the future cause you can read like help the comment or your partners that are working with you.
-you can commento a line using // or more than one line using the form to comment this answer*/
+you can comment a line using // or more than one line using the form to comment this answer*/
 //3.-The files in the fmt package would begin with package fmt
 //4.-We have to import os package then statement the function Exit with "os.Exit()"
 //5.- package main import ("fmt) func main (){fmt.Println("Hello my name is Alexander")}
-func main() {
-	fmt.Println("Hello, World")
-
-}
