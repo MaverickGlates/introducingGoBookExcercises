3169bc0369e63d1cc674805c57e76ff2c36f3af7
Alexander G <alexander.maverick1996@gmail.com>
2022-02-17 12:56:46 -0300

    Slices Chapter 5

--- chapter5/main.go
+++ chapter5/main.go
@@ -96,8 +96,50 @@ func main() {
 		//83,
 	}
 
-	/* Because the length of an array is part of its type name, working with arrays can be a little cumbersome. Adding or removing elements as we did here requires also changing
+	/* Because the length of an array is part of it's type name, working with arrays can be a little cumbersome. Adding or removing elements as we did here requires also changing
 	the length inside the brackets. Because of this and other limitations, you rarely see arrays used directly in Go code. Instead, you will usually use a slice, which is a type built on
 	top of an array.*/
+
+	//SLICES
+
+	// A slice is a segment of an array. Like arrays, slices are indexable and have a length. Unlike arrays, this length is allowed to change. Here's an example of a slice:
+
+	var ax []float64
+	fmt.Println(len(ax)) //The only difference between slices and an arrays is the missing length between the brackets. In this case, x has been created with a length of zero.
+	// have this in mind, all that you can do with an array, you can do it with a slices with one or two exceptions.
+
+	//If you want to create a slice, you should use the built-in make function like this:
+
+	ay := make([]float64, 5)
+	fmt.Println(len(ay)) /* This creates a slice that is associated with an underlying float64 array of length 5. Slices are always associated with some array, and although they can never
+	be longer than the array, they can be smaller. The make function also allows a third parameter: */
+
+	thirdParameter := make([]float64, 5, 10)
+	fmt.Println(len(thirdParameter))
+	fmt.Println(cap(thirdParameter))
+	/* How we said before a slice basically is a underlying from an array, now there's an additional function that we have available with slices, the capacity, that's because the the number
+	of elements in the slices doesn't necessarily match the size of the backing array, because the slice is really a projection of that underlaying array, this mean that we work with an
+	array indirectly, imagine we have a slice length 5 and back it an array length 10, this is exactly example of the representation before in line 117 code. In resume, we can have statement
+	a slice and define a length that not necessarily is his max length, because we can define a capacity that matching with the underlying array length*/
+
+	//Another way to create slices is to use the [low : high] expression:
+
+	/* low is the index of where to start the slice, high is the index of where to end it (but not including the index itself). For example, while arr[0:5] returns
+	[1,2,3,4,5], arr[1:4] returns [2,3,4] */
+
+	arr := [5]float64{1, 2, 3, 4, 5}
+	xArr := arr[1:4] //[Low expression include his element: High expression don't include it] we see 1 is the second element (2) and 4 is the fifth element (5), the print just show us [2 3 4]
+	fmt.Println(xArr)
+
+	/*For convenience, we are also allowed to omit low, high, or even both low and high. arr[0:] is the same as arr[0:len(arr)], arr[:5] is the same as arr[0:5], and
+	  arr[:] is the same as arr[0:len(arr)].*/
+
+	xArr = arr[0:len(arr)]
+	fmt.Println(xArr)
+
+	xArr = arr[:]
+	fmt.Println(xArr)
+
+	//In addition to the indexing operator, Go includes two built-in functions to assist with slices: append and copy
 
 }
